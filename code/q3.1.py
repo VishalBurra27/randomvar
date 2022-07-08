90ee73bbf6e2db4f7004a4ce32bdcb8be6635c45
VishalBurra27 <vishalmathews2004@gmail.com>
2022-07-08 12:06:02 +0530

    v8

--- code/q3.1.py
+++ code/q3.1.py
@@ -1,12 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy
+import math
 x = np.linspace(-10,10,50) #points on the x axis
-x = np.linspace(-10,10,50)#points on the x axis
-x1 = np.linspace(-10,0,50)
-y1 = np.zeros(50)
-x2 = np.linspace(0,10,50)
-y2 = 1 - np.exp(-x2/2)
+
+def logi(x):
+    if x<0:
+        return 0
+    else:
+        return 1-(math.exp(-x/2))
+logcdf=scipy.vectorize(logi,otypes=['double'])
+
+
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 #randvar = np.random.normal(0,1,simlen)
@@ -17,10 +22,11 @@ for i in range(0,50):
     err_n = np.size(err_ind) #computing the probability
     err.append(err_n/simlen) #storing the probability values in a list
 
-plt.plot(x,err,'o')#plotting the CDF
-plt.plot(np.concatenate([x1,x2]),np.concatenate([y1,y2]),color="orange")
+plt.grid()
+plt.plot(x.T,err,"o")
+plt.plot(x,logcdf(x),color="orange")
 
-plt.grid() #creating the grid
+
 plt.xlabel('$v$')
 plt.ylabel('$F_V(v)$')
 plt.legend(["Numerical","Theory"])
