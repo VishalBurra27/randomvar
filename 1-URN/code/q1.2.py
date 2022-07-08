025282ecb91bb82b1371aa6069a7efa6036eed2e
VishalBurra27 <vishalmathews2004@gmail.com>
2022-07-08 11:13:25 +0530

    v7

--- 1-URN/code/q1.2.py
+++ 1-URN/code/q1.2.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy
 x = np.linspace(-4,4,80)#points on the x axis
 #Theory graph
 #Obtained Theory graph points
-left = np.zeros(40)
-main = np.linspace(0,1,10)
-right = np.ones(30)
-ty = np.concatenate([left,main, right])
+
+
+def uniCdf(x):
+    if (x < 0): return 0
+    elif (0 <= x < 1):
+        return x*1.0
+    else: return 1
+func = scipy.vectorize(uniCdf, otypes=['double'])
+
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 #randvar = np.random.normal(0,1,simlen)
@@ -20,7 +25,7 @@ for i in range(0,80):
 
 plt.plot(x.T,err,'o',)#plotting the CDF
 
-plt.plot(x,ty,color="orange")
+plt.plot(x,func(x),color="orange")
 
 plt.grid() #creating the grid
 plt.xlabel('$x$')
